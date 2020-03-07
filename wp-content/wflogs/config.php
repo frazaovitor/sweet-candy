0612b0c8d774336045f3050bbf51224f004c2435
Vitor Frazão <frazaovitor@gmail.com>
2020-03-07 07:36:34 -0300

    atualizando plugins e alguns detalhes

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1584148677;s:7:"authKey";s:64:"Ct^e6,)^Y L>Dn OEnABc&o^r}]Yb(MVvXP9_Pzqqi*OHJc0}:PpJOCkZ#so7lFH";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:2004;}
