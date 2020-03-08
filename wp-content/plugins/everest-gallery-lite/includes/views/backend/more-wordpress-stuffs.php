7a4593b73f62b7927f2bbb47db3bb3f42b3b2397
Vitor Frazão <frazaovitor@gmail.com>
2020-03-08 18:55:40 -0300

    atualizando pra upar online

---
+++ wp-content/plugins/everest-gallery-lite/includes/views/backend/more-wordpress-stuffs.php
@@ -0,0 +1,37 @@
+<div class="eg-wrap wrap">
+    <div class="eg-header-wrap">
+        <h3>
+            <span class="eg-admin-title"><?php _e('More WP Resources', 'everest-gallery-lite'); ?></span>
+        </h3>
+        <div class="logo">
+            <img src="<?php echo EGL_IMG_DIR . 'logo.png'; ?>"/>
+        </div>
+    </div>
+    <div class="eg-form-wrap">
+        <div class="eg-content-wrap">
+            <h4> Themes Compatible with the Plugin :</h4>
+
+            <p><strong>Everest Gallery Lite</strong> works best with every WordPress theme. It's even more remarkable when used with popular themes like VMagazine and AccessPress Parallax.</p>
+
+            <p>AND IF THIS PLUGIN HAS IMPRESSED YOU, THEN YOU WOULD ENJOY OUR OTHER PROJECTS TOO. DO CHECK THESE OUT :</p>
+
+            <p><a href="https://wpall.club/">WPAll Club</a> -  A complete WordPress resources club. WordPress tutorials, blogs, curated free and premium themes and plugins, WordPress deals, offers, hosting info and more.</p>
+
+            <p> <a href="https://themeforest.net/user/accesskeys/portfolio">Premium WordPress Themes</a> -   6 premium WordPress themes well suited for all sort of websites. Professional, well coded and highly configurable themes for you. </p>
+
+            <p>  <a href="https://codecanyon.net/user/accesskeys/portfolio?Ref=AccessKeys">Premium WordPress Plugins</a> - 45+ premium WordPress plugins of many different types. High user ratings, great quality and best sellers in CodeCanyon marketplace. </p>
+
+            <p>  <a href="https://accesspressthemes.com/">AccessPress Themes</a> - AccessPress Themes has 50+ beautiful and elegant, fully responsive, multipurpose themes to meet your need for free and commercial basis.</p>
+
+            <p>  <a href="https://8degreethemes.com/">8Degree Themes</a> - 8Degree Themes offers 15+ free WordPress themes and 16+ premium WordPress themes carefully crafted with creativity.</p>
+        </div>
+    </div>
+</div>
+<div class="eg-image-wrapper"> 
+        <img src="<?php echo EGL_IMG_DIR.'upgrade-top.png';?>"/>
+        <a href="http://demo.accesspressthemes.com/wordpress-plugins/everest-gallery/" class="eg-ref-btn eg-color" target="_blank"><?php _e('Demo','everest-gallery-lite');?></a>
+        <a href="https://1.envato.market/c/1302794/275988/4415?u=https%3A%2F%2Fcodecanyon.net%2Fitem%2Feverest-gallery-responsive-wordpress-gallery-plugin%2F20067428"  target="_blank" class="eg-ref-btn"><?php _e('Buy Now','everest-gallery-lite');?></a>
+        <img src="<?php echo EGL_IMG_DIR.'upgrade-bottom.png';?>"/>
+        <a href="http://demo.accesspressthemes.com/wordpress-plugins/everest-gallery/" class="eg-ref-btn eg-color" target="_blank"><?php _e('Demo','everest-gallery-lite');?></a>
+        <a href="https://1.envato.market/c/1302794/275988/4415?u=https%3A%2F%2Fcodecanyon.net%2Fitem%2Feverest-gallery-responsive-wordpress-gallery-plugin%2F20067428"  target="_blank" class="eg-ref-btn"><?php _e('Buy Now','everest-gallery-lite');?></a>
+ </div>
